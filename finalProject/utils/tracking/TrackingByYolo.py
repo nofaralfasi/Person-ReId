2cbe6572dce3fb2c587110e53e7280cc11d81c56
nofar <nofaralfasi@gmail.com>
2019-12-16 12:16:23 +0200

    updated createDescriptorSource

--- finalProject/utils/tracking/TrackingByYolo.py
+++ finalProject/utils/tracking/TrackingByYolo.py
@@ -98,6 +98,7 @@ def TrackingByYolo(sequences: [], yolo, isVideo: bool, config: "file"):
 
     return myPeople
 
+# sequences is all frames related to the source
 def SourceDetectionByYolo(sequences: [], yolo, isVideo: bool, config: "file"):
     frameCounter = 0
     frameRate = config["frameRate"]
@@ -111,7 +112,7 @@ def SourceDetectionByYolo(sequences: [], yolo, isVideo: bool, config: "file"):
         numOfFrames = len(sequences)
 
     if numOfFrames > 1:
-        # start capture, looping on each frame
+        # start capture, looping on the frames, skipping the frameRate
         for index in range(0, numOfFrames, frameRate):
             print("frame {}".format(index))
             if isVideo:
@@ -123,37 +124,34 @@ def SourceDetectionByYolo(sequences: [], yolo, isVideo: bool, config: "file"):
             croppedImage = yolo.forward(frame2)
             croppedImage = list(filter(lambda crop: crop["frame"].size, croppedImage))
             print("list of detection", len(croppedImage))
-            if index == 0:
-                # first frame
-                for c in croppedImage:
+
+            # index is the frame number
+            for c in croppedImage:
+                if index == 0: # first frame
                     human = Human(frameCounter)
                     frameCounter += 1
-                    human.frames.append(c["frame"])
-                    human.locations.append(c["location"])
-            elif index > 0:
-                # not the first frame
-                for c in croppedImage:
-                    if len(human.frames) > 0:
-                        maxMatch = findSourceFeatures(c, human, config=config)
-                        if maxMatch is None:
-                            print("couldn't find features...")
-                            continue
-
+                human.frames.append(c["frame"])
+                human.locations.append(c["location"])
+                if index > 0: # not the first frame
+                    maxMatch = findSourceFeatures(c, human, config=config)
+                    if maxMatch is None:
+                        print("couldn't find features...")
+                        continue
+                    else:
                         element = max(maxMatch, key=lambda item: item[1])
-                        # cv2.imshow('targetFromMovie', c["frame"])
                         print('My Source Score: ', element[1])
-                        if element[1] > config["thresholdAppendToHuman"]:  # score match
-                            # TODO check if we want to save this frame?
-                            # cv2.imshow('scoreHumanImageFromMyPeople', element[0].frames[-1])
-                            human.frames.append(c["frame"])
-                            human.locations.append(c["location"])
-
+                        # TODO check if we want to save this frame?
+                        # if element[1] > config["thresholdAppendToHuman"]:  # score match
+                        #     human.frames.append(c["frame"])
+                        #     human.locations.append(c["location"])
             DrawSource(human, drawFrame)
             # find ids from previous frame
             cv2.imshow('frame', drawFrame)
             k = cv2.waitKey(config["WaitKeySecond"]) & 0xff
             if k == 27:
                 break
+    else:
+        print("The number of frames is less than one")
 
     # print("number of people ", len(myPeople))
     # for index, p in enumerate(myPeople):
