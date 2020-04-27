620b45272cc68eddcea966debe6f852a629eb8b1
nofar <nofar.alfasi@s.afeka.ac.il>
2020-04-27 16:42:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ pip install opencv-contrib-python==3.4.2.16
 
 import json
 import pprint
+import numpy as np
+import cv2 as cv
 
 from finalProject.classes.yolo import Yolo
-from finalProject.utils.drawing.draw import draw_final_results
+from finalProject.utils.drawing.draw import draw_final_results, draw_rects
 from finalProject.utils.keyPoints.AlgoritamKeyPoints import create_keypoints_descriptors
 from finalProject.utils.matchers.Matchers import compute_accuracy_table
 from finalProject.utils.preprocessing.preprocess import read_frames_from_video, check_frames_exist, reduce_noise, removeRemovalColor
 from finalProject.utils.tracking.TrackingByYolo import source_detection_by_yolo, tracking_by_yolo
+from finalProject.classes.results_details import ResultsDetails
+from finalProject.utils.cascade.cascade_detect  import detect
+
+
 
 if __name__ == "__main__":
     """# import images"""
@@ -22,11 +28,30 @@ if __name__ == "__main__":
     with open('./config.txt') as file_json:
         config = json.load(file_json)
 
+        src_input_data = ResultsDetails(config["source"])
+        trg_input_data = ResultsDetails(config["target"])
+
         """ source video """
         source_frames = read_frames_from_video(config["source"])  # a list of all frames extracted from source video
         if not check_frames_exist(source_frames):  # if not len(source_frames) > 0
             print("problem with source video input")
             exit(0)
+
+        if config["source"]["Cascade"]:
+            cascade = cv.CascadeClassifier('C:\\opencv\\\sources\\data\\haarcascades\\haarcascade_lowerbody.xml')
+            for img in source_frames:
+                gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+                gray = cv.equalizeHist(gray)
+
+                rects = detect(gray, cascade)
+                print(len(rects))
+                vis = img.copy()
+                draw_rects(vis, rects, (0, 255, 0))
+                cv.imshow('cascade_detect', vis)
+
+                k = cv.waitKey(5)
+                if k == 27:
+                    cv.destroyAllWindows()
 
         # preprocessing reduce noise background
         if config["source"]["reduceNoise"]:
@@ -38,11 +63,7 @@ if __name__ == "__main__":
         if config["source"]["removeRemovalColor"]:
             source_frames = removeRemovalColor(source_frames)
 
-        # for frame in source_frames:
-        #     cv2.imshow('removeRemovalColor frame', frame)
-        #     keyboard = cv2.waitKey(30)
-        #     if keyboard == 'q' or keyboard == 27:
-        #         break
+        src_input_data.num_of_frames = len(source_frames)
 
         source_person = source_detection_by_yolo(source_frames, yolo, is_video=config["source"]["isVideo"], config=config["source"])
         if source_person is None:
@@ -68,6 +89,8 @@ if __name__ == "__main__":
         if not check_frames_exist(target_frames):
             print("problem with target video input - in remove color")
             exit(0)
+
+        trg_input_data.num_of_frames = len(target_frames)
 
         target_people = tracking_by_yolo(target_frames, yolo, is_video=config["target"]["isVideo"], config=config["target"])
         if not check_frames_exist(target_people):
