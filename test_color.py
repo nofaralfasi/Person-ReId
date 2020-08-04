b38a5fb6c8ff4c0f519c47c7c06da94fdb05c8ae
nofar <nofar.alfasi@s.afeka.ac.il>
2020-08-04 18:49:20 +0300

    Delete test_color.py

--- test_color.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import cv2
-
-
-import cv2
-image = cv2.imread("dataset/images/color.png")
-plt.axis("off")
-plt.imshow(cv2.cvtColor(image,cv2.COLOR_BGR2RGB))
-plt.show()
