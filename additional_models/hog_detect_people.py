c4ed803f7fc86dd0c7670b7d6f0a97c1061a0b93
nofar <nofar.alfasi@s.afeka.ac.il>
2020-08-04 20:12:47 +0300

    Create hog_detect_people.py

---
+++ additional_models/hog_detect_people.py
@@ -0,0 +1,43 @@
+import cv2
+import imutils
+
+# Initializing the HOG person
+# detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+cap = cv2.VideoCapture('C:/Users/HP/PycharmProjects/FinalProject-ReId/dataset/re-id/videos/home.mp4')
+
+while cap.isOpened():
+    # Reading the video stream
+    ret, image = cap.read()
+    if ret:
+        image = imutils.resize(image,
+                               width=min(600, image.shape[1]))
+
+        # Detecting all the regions
+        # in the Image that has a
+        # pedestrians inside it
+        (regions, _) = hog.detectMultiScale(image,
+                                            winStride=(3, 3),
+                                            padding=(2, 2),
+                                            scale=1.05)
+        print("regs:", len(regions))
+
+        # Drawing the regions in the
+        # Image
+        for (x, y, w, h) in regions:
+            (x, y, w, h) = (x+30, y+30, w-60, h-60)
+            cv2.rectangle(image, (x, y),
+                          (x + w, y + h),
+                          (0, 0, 255), 2)
+
+            # Showing the output Image
+        cv2.imshow("Image", image)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
